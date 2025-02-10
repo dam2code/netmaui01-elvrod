e7fc33aaddbe7e155f1b63fff4507ff1ec7dc575
elvis Rodriguez <elvis.rodriguezsantos@educa.maddrid.org>
2025-02-10 12:31:41 +0100

    Primera Pagina XAML

--- Maui_app/MainPage.xaml.cs
+++ Maui_app/MainPage.xaml.cs
@@ -1,55 +1,64 @@
 ﻿
-namespace Phoneword;
+
+namespace Notes;
 
 public partial class MainPage : ContentPage
 {
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
     public MainPage()
     {
         InitializeComponent();
-    }
-    string translatedNumber;
+                editor = this.FindByName<Editor>("editor");
 
-    private void OnTranslate(object sender, EventArgs e)
-    {
-        string enteredNumber = PhoneNumberText.Text;
-        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
 
-        if (!string.IsNullOrEmpty(translatedNumber))
+        if (File.Exists(_fileName))
         {
-            CallButton.IsEnabled = true;
-            CallButton.Text = "Call " + translatedNumber;
+            editor.Text = File.ReadAllText(_fileName);
         }
-        else
-        {
-            CallButton.IsEnabled = false;
-            CallButton.Text = "Call";
-        }
-    }
-    async void OnCall(object sender, System.EventArgs e)
-    {
-        if (await this.DisplayAlert(
-       "Dial a Number",
-       "Would you like to call " + translatedNumber + "?",
-       "Yes",
-       "No"))
-        {
-            try
-            {
-                if (PhoneDialer.Default.IsSupported)
-                    PhoneDialer.Default.Open(translatedNumber);
-            }
-            catch (ArgumentNullException)
-            {
-                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
-            }
-            catch (Exception)
-            {
-                // Other error has occurred.
-                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
-            }
-        }
-    }
     
+
+    var notesHeading = new Label() { Text = "Notes", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold };
+
+        var buttonsGrid = new Grid() { HeightRequest = 40.0 };
+        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30.0, GridUnitType.Absolute) });
+        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+
+        var saveButton = new Button() { WidthRequest = 100, Text = "Save" };
+        saveButton.Clicked += OnSaveButtonClicked;
+        Grid.SetColumn(saveButton, 0);
+        buttonsGrid.Children.Add(saveButton);
+
+        var deleteButton = new Button() { WidthRequest = 100, Text = "Delete" };
+        deleteButton.Clicked += OnDeleteButtonClicked;
+        Grid.SetColumn(deleteButton, 2);
+        buttonsGrid.Children.Add(deleteButton);
+
+        var stackLayout = new VerticalStackLayout
+        {
+            Padding = new Thickness(30, 60, 30, 30),
+            Children = { notesHeading, editor, buttonsGrid }
+        };
+
+        this.Content = stackLayout;
+    }
+
+    private void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editor.Text);
+        DisplayAlert("Success", "Note saved!", "OK");
+    }
+
+    private void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+            editor.Text = string.Empty;
+            DisplayAlert("Deleted", "Note deleted!", "OK");
+        }
+    }
 }
 
 
